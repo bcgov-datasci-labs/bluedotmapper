3675c0841467bfb0a928c0b4dffb06a9b94d5404
Andy Teucher <andy.teucher@gov.bc.ca>
2019-11-08 13:11:35 -0800

    more attempts

--- inst/scratch.R
+++ inst/scratch.R
@@ -8,8 +8,17 @@ gdal_utils("info", dem)
 #works
 gdal_utils("info", "inst/DEM.tif")
 
-opts <- c("DPI=300")
+unlink("inst/dem.pdf")
+opts <- c("-of", "PDF", "-a_srs", "EPSG:3005", "-co", "DPI=300")
 
-gdal_utils("translate", "inst/DEM.tif", "inst/dem.pdf",
-           options = opts)
+gdal_utils("translate", "inst/DEM.tif", "inst/dem.pdf", options = opts)
+
+gdal_utils("info", "inst/dem.pdf")
+
+system("gdal_translate ")
+
+st_write(dem, "foo.pdf")
+
+raster("inst/dem.pdf")
+
 
